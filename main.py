1bf73c40682a8fffee769281fb34bef82fef936a
drdkslko81 <drdkslko81@gmail.com>
2026-01-03 22:45:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -711,11 +711,7 @@ def fetch_ios_reviews(app_id, country_code):
     """
 
     url = f"https://itunes.apple.com/{country_code}/rss/customerreviews/id={app_id}/sortBy=mostRecent/json"
-
-
     reviews = []
-
- 
 
     try:
 
